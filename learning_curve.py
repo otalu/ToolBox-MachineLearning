243f965e771c4c2140952d45690e51683dac285b
otalu <onurtalu@gmail.com>
2017-03-28 17:01:11 -0400

    Final version of the code

--- learning_curve.py
+++ learning_curve.py
@@ -1,4 +1,8 @@
-""" Exploring learning curves for classification of handwritten digits """
+"""
+Exploring learning curves for classification of handwritten digits
+
+Author: Onur Talu
+"""
 
 import matplotlib.pyplot as plt
 import numpy
@@ -18,29 +22,31 @@ def display_digits():
     plt.show()
 
 
-def train_model():
+def train_model(num_trials, LR):
     data = load_digits()
-    num_trials = 10
     train_percentages = range(5, 95, 5)
     test_accuracies = numpy.zeros(len(train_percentages))
-
-    # train models with training percentages between 5 and 90 (see
-    # train_percentages) and evaluate the resultant accuracy for each.
-    # You should repeat each training percentage num_trials times to smooth out
-    # variability.
-    # For consistency with the previous example use
-    # model = LogisticRegression(C=10**-10) for your learner
-
-    # TODO: your code here
-
+    for i in range(num_trials):
+        index = 0
+        for perc in train_percentages:
+            X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, train_size=perc/100)
+            model = LogisticRegression(C=LR)
+            model.fit(X_train, y_train)
+            test_accuracies[index] += model.score(X_train, y_train)
+            index += 1
+    test_accuracies = test_accuracies/num_trials
+    print("Train accuracy %f" %model.score(X_train, y_train))
+    print("Test accuracy %f"%model.score(X_test, y_test))
     fig = plt.figure()
     plt.plot(train_percentages, test_accuracies)
     plt.xlabel('Percentage of Data Used for Training')
     plt.ylabel('Accuracy on Test Set')
+    print(type(num_trials))
+    # plt.savefig('Trial%d.png' % num_trials)
+    plt.savefig('LR_Trials1.png')
     plt.show()
 
 
 if __name__ == "__main__":
-    # Feel free to comment/uncomment as needed
-    display_digits()
-    # train_model()
+    # don't forget to change the name of saved image in line 46
+    train_model(10, 10**-1)
